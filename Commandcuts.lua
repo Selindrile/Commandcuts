d6efdeab606bb4a06ae0aecd76c42ed74c0d3835
KalesAndRancor <some___guy@hotmail.com>
2025-02-19 23:46:48 -0600

    Added option to block non commands, simplified a few things.

--- Commandcuts.lua
+++ Commandcuts.lua
@@ -5,20 +5,33 @@ _addon.author = 'Selindrile'
 require 'strings'
 config = require 'config'
 
-default_commands = {
-	commands = S{'lua','gs','load','unload','cancel'}
+default_settings = {
+	commands = S{'lua','gs','load','unload','cancel'},
+	block_non_commands = false,
 }
 
-settings = config.load('data\\commands.xml',default_commands)
+settings = config.load('data\\settings.xml',default_settings)
 config.save(settings)
 setmetatable(settings,nil)
 commands = settings.commands
 
 windower.register_event('outgoing text',function(original,modified)
-	if original:startswith('/') and not original:startswith('//') then
-		local potentialcommand = string.match(original, "/(%S+)")
+	if original:startswith('/') then
+		if not original:startswith('//') then
+			local potentialcommand = string.match(original, "/(%S+)")
+			if commands[potentialcommand] then
+				windower.send_command(original:sub(2))
+				return true
+			end
+		end
+	else
+		local potentialcommand = string.match(original, "(%S+)")
 		if commands[potentialcommand] then
-			return '/'..original
+			windower.send_command(original)
+			return true
+		elseif settings.block_non_commands then
+			windower.add_to_chat(123, "Commandcuts: Message didn't start with a /, blocking.")
+			return true
 		end
 	end
 end)
